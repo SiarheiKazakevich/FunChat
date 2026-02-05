a305dca8daf25d94d113d09881e349f94fdd8cc6
Siarhei Kazakevich <dianrom@tut.by>
2026-02-05 23:05:31 +0300

    feat: finish production-ready boilerplate

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import unicorn from 'eslint-plugin-unicorn';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+      globals: {
+        console: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      unicorn,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'unicorn/prefer-module': 'off',
+    },
+  },
+
+  prettier,
+];

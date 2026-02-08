ef7e5cfc601d18cc8f7e49c368b040abff37eca7
Siarhei Kazakevich <dianrom@tut.by>
2026-02-08 20:47:28 +0300

    feat: add store for SPA

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,6 +2,7 @@ import { Router } from '../router/Router';
 import { LoginPage } from '../pages/LoginPage';
 import { MainPage } from '../pages/MainPage';
 import { AboutPage } from '../pages/AboutPage';
+import { store } from '../store/index';
 
 export class App {
   public start(): void {
@@ -13,6 +14,10 @@ export class App {
     router.register('/login', LoginPage);
     router.register('/', MainPage);
     router.register('/about', AboutPage);
+
+    store.subscribe((state) => {
+      console.log('STATE UPDATED:', state);
+    });
 
     router.start();
   }
--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -1,5 +1,15 @@
+import { store } from '../store/index';
+
 export function LoginPage(): HTMLElement {
-  const el = document.createElement('div');
-  el.textContent = 'Login page';
-  return el;
+  const btn = document.createElement('button');
+  btn.textContent = 'Fake login';
+
+  btn.onclick = () => {
+    store.setState({
+      user: { id: '1', name: 'John' },
+      isAuthorized: true,
+    });
+  };
+
+  return btn;
 }
